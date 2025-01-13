2be80bbaade6b915f9dfa590ee7f9006ea578032
ReiRev <reirev2913@gmail.com>
2025-01-13 06:29:48 +0000

    complete 16

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Models\User;
 use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
 
@@ -10,9 +11,8 @@ Route::get('/', function () {
 });
 
 Route::get('/users', function () {
-
     return Inertia::render('Users', [
-        'time' => now()->toTimeString()
+        'users' => User::all()->setVisible(['name', 'id'])
     ]);
 });
 
