66a9fbc16cbe65c6cd8427eab044224ce88a7f5d
ReiRev <reirev2913@gmail.com>
2025-01-13 04:43:29 +0000

    complete 13

--- resources/js/Pages/Settings.vue
+++ resources/js/Pages/Settings.vue
@@ -1,11 +1,3 @@
-<script setup lang="ts">
-import Layout from '@/Shared/Layout.vue'
-
-defineOptions({
-  layout: Layout,
-})
-</script>
-
 <template>
   <h1 class="text-4xl font-bold">Settings</h1>
 </template>
