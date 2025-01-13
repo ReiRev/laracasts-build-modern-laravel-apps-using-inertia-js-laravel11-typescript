cb22e8389d12d14f756bbe0c9ab1300e63be076e
ReiRev <reirev2913@gmail.com>
2025-01-13 06:07:28 +0000

    complete 15

--- resources/js/Shared/Layout.vue
+++ resources/js/Shared/Layout.vue
@@ -9,6 +9,10 @@ const username = computed(() => auth.user.username)
 </script>
 
 <template>
+  <Head>
+    <title>My App</title>
+    <meta type="description" content="Information about my app" head-key="description" />
+  </Head>
   <section class="p-6 bg-gray-200">
     <header class="flex justify-between">
       <div class="flex items-center">
@@ -21,14 +25,6 @@ const username = computed(() => auth.user.username)
 
   <section class="p-6">
     <div class="max-w-3xl mx-auto w-full">
-      <iframe
-        class="mb-6"
-        width="100%"
-        frameborder="no"
-        scrolling="no"
-        seamless
-        src="https://player.simplecast.com/fd0bd2ba-c553-466c-a060-b144797ce369?dark=false"
-      />
       <slot />
     </div>
   </section>
