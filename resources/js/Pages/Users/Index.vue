8fcd3c9a3fb00d35fa0e9c4dd29791432fb58c65
ReiRev <reirev2913@gmail.com>
2025-01-18 03:24:45 +0000

    complete 19

--- resources/js/Pages/Users/Index.vue
+++ resources/js/Pages/Users/Index.vue
@@ -22,7 +22,11 @@ const props = defineProps<{
 
 const search = ref(props.filters.search)
 watch(search, (value) => {
-  router.get('/users', { search: value }, { preserveState: true, replace: true })
+  router.get(
+    '/users',
+    { search: value },
+    { preserveState: true, replace: true },
+  )
 })
 </script>
 
@@ -32,13 +36,22 @@ watch(search, (value) => {
   <div class="flex justify-between mb-6">
     <h1 class="text-3xl font-bold">Users</h1>
 
-    <input v-model="search" type="text" placeholder="Search..." class="border px-2 rounded-lg" />
+    <input
+      v-model="search"
+      type="text"
+      placeholder="Search..."
+      class="border px-2 rounded-lg"
+    />
   </div>
 
   <div class="flex flex-col">
     <div class="-my-2 overflow-x-auto sm:-mx-6 lg:-mx-8">
-      <div class="py-2 align-middle inline-block min-w-full sm:px-6 lg:px-8">
-        <div class="shadow overflow-hidden border-b border-gray-200 sm:rounded-lg">
+      <div
+        class="py-2 align-middle inline-block min-w-full sm:px-6 lg:px-8"
+      >
+        <div
+          class="shadow overflow-hidden border-b border-gray-200 sm:rounded-lg"
+        >
           <table class="min-w-full divide-y divide-gray-200">
             <tbody class="bg-white divide-y divide-gray-200">
               <tr v-for="user in users.data" :key="user.id">
@@ -51,7 +64,9 @@ watch(search, (value) => {
                     </div>
                   </div>
                 </td>
-                <td class="px-6 py-4 whitespace-nowrap text-right text-sm font-medium">
+                <td
+                  class="px-6 py-4 whitespace-nowrap text-right text-sm font-medium"
+                >
                   <Link
                     :href="`/users/${user.id}/edit`"
                     class="text-indigo-600 hover:text-indigo-900"
